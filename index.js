473ca93e94b961598f9533038855959961b8aa6c
Nithin Modiboyana <129368739+iamNithiiN@users.noreply.github.com>
2023-12-12 16:39:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ img2.setAttribute('src', newSrc2);
 let h1 = document.querySelector('h1');
 
 if (randomNumber1 > randomNumber2) {
-    h1.textContent = 'Player 1 Wins! 🚩';
+    h1.textContent = '🚩Player 1 Wins!';
 } else if (randomNumber1 < randomNumber2) {
-    h1.textContent = '🚩Player 2 Wins!';
+    h1.textContent = 'Player 2 Wins!🚩';
 } else {
     h1.textContent = 'It\'s a tie 🪢!';
 }
